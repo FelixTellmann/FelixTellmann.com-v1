e2140852e7f35a21fc08008b7f38f810e24e1ec9
FelixTellmann <Fr10tz1!1>
2020-04-15 13:58:51 +0200

    Initial commit

---
+++ components/Border/Border.tsx
@@ -0,0 +1,34 @@
+import React, { FC, useState, useEffect } from 'react';
+import './Border.scss';
+
+type BorderProps = {
+  loading: boolean
+  duration: number
+  width: string
+  color: string
+}
+
+export const Border: FC<BorderProps> = ({ loading, duration, width, color }) => {
+  const [loop, setLoop] = useState(loading ? 'infinite' : 1);
+  const style: any = {
+    '--border-width': `${width}`,
+    '--border-color': `${color}`,
+    '--border-animation-delay': `${-duration / 2}s`,
+    '--border-animation-duration': `${duration}s`,
+    '--border-animation-loop': `${loop}`,
+  };
+
+  const updateLoadingStopper = (e) => {
+    loading || setLoop(e.elapsedTime / duration);
+  };
+
+  useEffect(() => setLoop(loading ? 'infinite' : loop));
+
+
+  return (
+    <div style={style} className="border" onAnimationIteration={updateLoadingStopper} />
+  );
+};
+
+
+
