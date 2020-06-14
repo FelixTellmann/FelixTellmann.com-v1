efbe4188f82abfa43be33c5242e622797bc2f74c
FelixTellmann <Fr10tz1!1>
2020-06-14 19:02:41 +0200

    Added Blog Page & .md parsing

--- components/Section/Hero/Hero.tsx
+++ components/Section/Hero/Hero.tsx
@@ -1,35 +1,32 @@
 import React, { FC, useEffect, useRef } from "react";
-import './Hero.scss';
-import { Mouse } from 'components/Icons';
-import { Loading, Typed } from 'components';
+import { Mouse } from "components/Icons";
+import { Typed } from "components";
+import "./Hero.scss";
 
 export const Hero: FC = () => {
-
-    const content = [
-        `I build beautiful <br>websites.`,
-        /*`I help people <br>succeed online.`,
-        `I teach &amp; write <br>about code.`,*/
-    ];
-
-    return (
-        <section className="hero">
-            <div className="hero__content">
-                <h1 className="hero">
-                    Hi, I'm <span className="highlight">Felix</span>. <br />
-                    <Typed content={content}
-                           typeSpeed={40}
-                           backSpeed={17}
-                           backDelay={1850}
-                           loop={true}
-                           cursorChar={'|'} />
-                </h1>
-{/*                <div className="hero__cta">
-                    <Button>Get in Touch</Button>
-                </div>*/}
-            </div>
-            <a href="#" className="scroll-down">
-                <Mouse />
-            </a>
-        </section>
-    );
+  
+  const content = [
+    `I build beautiful <br>websites.`,
+    /*`I help people <br>succeed online.`,
+    `I teach &amp; write <br>about code.`,*/
+  ];
+  
+  return (
+    <section className="hero">
+      <div className="hero__content">
+        <h1 className="hero">
+          Hi, I'm <span className="highlight">Felix</span>. <br />
+          <Typed content={content}
+                 typeSpeed={40}
+                 backSpeed={17}
+                 backDelay={1850}
+                 loop={true}
+                 cursorChar={"|"} />
+        </h1>
+      </div>
+      <a href="#" className="scroll-down">
+        <Mouse />
+      </a>
+    </section>
+  );
 };
